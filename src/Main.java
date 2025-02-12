e43bc666c3f82fcbb8664d9dd27613bba8ba9b85
Levi-JDK <geraza2427@gmail.com>
2025-02-12 16:30:20 -0500

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            Semaforo semaforo1 = new Semaforo("rojo");
+            String continuar;
+            String reset = "\u001B[0m";
+            boolean hc = true;
+            do {
+                semaforo1.mostrarEstado();
+                System.out.println(reset + "1. Dar paso a verticales");
+                System.out.println("2. Dar paso a horizontales");
+                System.out.println("3. Apagar Semaforos");
+                System.out.println("Ingrese el numero de la opcion deseada: ");
+                int estSemaforo = sc.nextInt();
+                switch (estSemaforo){
+                    case 1:
+                        if(semaforo1.getEstado()== "rojo"){
+                        semaforo1.setEstado("amarillo");
+                        semaforo1.mostrarEstado();
+                        semaforo1.setEstado("verde");}
+                        break;
+                    case 2:
+                        if(semaforo1.getEstado()=="verde"){
+                        semaforo1.setEstado("amarillo");
+                        semaforo1.mostrarEstado();
+
+                        semaforo1.setEstado("rojo");
+                        }
+                        break;
+                    case 3:
+                        hc = false;
+                        break;
+                }
+            } while (hc);
+            sc.close();
+            System.out.println("Apagando semaforos...");
+            System.out.println("Bye :)");
+        }
 }
+
